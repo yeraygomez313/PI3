6082210f32da489e68d9037d6ac9676f4bd77b6d
LinKraDG <gabriel.vidfu@gmail.com>
2025-05-19 19:31:02 +0200

    Created scriptable objects for habilities

---
+++ Assets/MyAssets/ScriptObjects/HabilityStats.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HabilityStats", menuName = "PI3/HabilityStats")]
+public class HabilityStats : ScriptableObject
+{
+    public string habilityName;
+    public float instantDamage;
+    public float tickDamage;
+    public float cooldown;
+    public float duration;
+}
