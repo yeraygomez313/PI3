2363dadc7b851669054ff5952f185b97155b5b66
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2025-05-20 16:39:33 +0200

    Folder rearrangement

---
+++ Assets/MyAssets/Scripts/UI/Core/CardVisuals.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardVisuals : MonoBehaviour
+{
+    [SerializeField] private Image cardIcon;
+
+    public void SetCard(MonsterStats monsterStats)
+    {
+        cardIcon.sprite = monsterStats.Icon;
+    }
+}
